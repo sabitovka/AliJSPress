28019142945016c818b790ffad38f82500811a27
Karim Sabitov <karim13.sab@gmail.com>
2020-01-26 19:44:54 +0300

    Third day webinar

--- script/script.js
+++ script/script.js
@@ -6,6 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const cart = document.querySelector('.cart');
   const goodsWrapper = document.querySelector('.goods-wrapper');
   const category = document.querySelector('.category');
+  const cardCounter = cartBtn.querySelector('.counter');
+  const wishlistCounter = wishlistBtn.querySelector('.counter');
+  const cartWrapper = document.querySelector('.cart-wrapper');
+
+  const wishList = [];
+
+  let goodsBacket = {};
+
+  const loading = () => {
+    goodsWrapper.innerHTML = `<div id="spinner"><div class="spinner-loading"><div><div><div>
+    </div></div><div><div></div></div>
+    <div><div></div></div><div><div></div></div></div></div></div>`;
+  };
 
   const createCardGoods = (id, title, price, img) => {
      const card = document.createElement('div');
@@ -13,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
      card.innerHTML = `<div class="card">
                          <div class="card-img-wrapper">
                            <img class="card-img-top" src="${img}" alt="">
-                           <button class="card-add-wishlist"
+                           <button class="card-add-wishlist ${wishList.includes(id) ? 'active' : ''}"
                               data-goods-id="${id}"></button>
                          </div>
                          <div class="card-body justify-content-between">
@@ -36,14 +49,17 @@ document.addEventListener('DOMContentLoaded', () => {
       document.removeEventListener('keydown', closeCart);
   };
 
+  const showCartBacket = goods => goods.filter(item => goodsBasket.hasOwnerProperty(item.id));
+
   const openCart = () => {
     event.preventDefault();
     cart.style.display = 'flex';
     document.addEventListener('keydown', closeCart);
+    getGoods(renderCartBasket, showCartBacket);
   };
 
   const getGoods = (handler, filter) => {
-    goodsWrapper.textContent = "";
+    loading();
     fetch("db/db.json")
       .then(response => response.json())
       .then(filter)
@@ -51,10 +67,55 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   const renderCard = (item) => {
-    item.forEach(({ id, title, price, imgMin }) => {
-      goodsWrapper.appendChild(createCardGoods(id, title, price, imgMin));
-    })
+    goodsWrapper.textContent = "";
+
+    if (item.length) {
+      item.forEach(({ id, title, price, imgMin }) => {
+        goodsWrapper.appendChild(createCardGoods(id, title, price, imgMin));
+      });
+    } else {
+      goodsWrapper.textContent = 'Извините мы не нашли товаров по вашему запросу!';
+    }
   };
+
+  //  рендер корзины
+
+  const createCartGoodsBasket = (id, title, price, img) => {
+     const cart = document.createElement('div');
+     cart.className = 'goods';
+     cart.innerHTML = `<div class="goods-img-wrapper">
+                         <img class="goods-img" src="${img}" alt="">
+
+                       </div>
+                       <div class="goods-description">
+                         <h2 class="goods-title">${title}</h2>
+                         <p class="goods-price">${price} ₽</p>
+
+                       </div>
+                       <div class="goods-price-count">
+                         <div class="goods-trigger">
+                           <button class="goods-add-wishlist" data-goods-id="${id}"></button>
+                           <button class="goods-delete" data-goods-id="${id}"></button>
+                         </div>
+                      <div class="goods-count">1</div>`;
+
+       return cart;
+  };
+
+  const renderCartBasket = (item) => {
+    cartWrapper.textContent = "";
+
+    if (item.length) {
+      item.forEach(({ id, title, price, imgMin }) => {
+        cartWrapper.appendChild(createCartGoodsBasket(id, title, price, imgMin));
+      });
+    } else {
+      cartWrapper.innerHTML = '<div id="cart-empty">Ваша корзина пока пуста</div>';
+    }
+  };
+
+
+
 
   const randomSort = (items) => items.sort(() => Math.random() - 0.5);
 
@@ -64,15 +125,104 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (target.classList.contains('category-item')) {
       const category = target.dataset.category;
-      getGoods(renderCard, goods =>
-        goods.filter(item =>
-          item.category.includes(category)))
+      getGoods(renderCard, goods => goods.filter(item => item.category.includes(category)))
     }
+  };
+
+  const searchGoods = event => {
+    event.preventDefault();
+
+    const input = event.target.elements.searchGoods;
+    const inputValue = input.value.trim();
+    if (inputValue !== '') {
+      const searchString = new RegExp(inputValue, 'i');
+      getGoods(renderCard, goods => goods.filter(item => searchString.test(item.title)));
+    } else {
+      getGoods(renderCard, randomSort);
+    }
+  };
+
+  const getCookie = name => {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+
+  const cookieQuery = get => {
+    if (get) {
+      goodsBacket = JSON.parse(getCookie('goodsBasket'));
+    } else {
+      document.cookie = `goodsBasket=${JSON.stringify(goodsBacket)};max-age=86400e3`
+    }
+    console.log(goodsBacket);
+    checkCount();
+  };
+
+  const checkCount = () => {
+    wishlistCounter.textContent = wishList.length;
+    cardCounter.textContent = Object.keys(goodsBacket).length;
+  };
+
+  const storageQuery = get => {
+    if (get) {
+      if (localStorage.getItem('wishlist')) {
+        JSON.parse(localStorage.getItem('wishlist'))
+          .forEach(id => wishList.push(id));
+      }
+    } else {
+      localStorage.setItem('wishlist', JSON.stringify(wishList));
+    }
+    checkCount();
+  }
+
+  const toggleWishList = (id, elem) => {
+
+    if (wishList.includes(id)) {
+      wishList.splice(wishList.indexOf(id), 1);
+      elem.classList.remove('active');
+    }else {
+      wishList.push(id);
+      elem.classList.add('active');
+    }
+
+    storageQuery();
+  };
+
+  const addBasket = id => {
+    if (goodsBacket[id]) {
+      goodsBacket[id] += 1;
+    } else {
+      goodsBacket[id] = 1;
+    }
+    console.log(goodsBacket);
+    cookieQuery();
+  };
+
+  const handlerGoods = event => {
+    const target = event.target;
+
+    if (target.classList.contains('card-add-wishlist')) {
+      toggleWishList(target.dataset.goodsId, target);
+    };
+
+    if (target.classList.contains('card-add-cart')) {
+      addBasket(target.dataset.goodsId);
+    }
+  };
+
+  const showWishList = () => {
+    getGoods(renderCard, goods => goods.filter(item => wishList.includes(item.id)));
   };
 
   cartBtn.addEventListener('click', openCart);
   cart.addEventListener('click', closeCart);
   category.addEventListener('click', choiceCategory);
+  search.addEventListener('submit', searchGoods);
+  goodsWrapper.addEventListener('click', handlerGoods);
+  wishlistBtn.addEventListener('click', showWishList);
 
   getGoods(renderCard, randomSort);
+  storageQuery('get');
+  cookieQuery('get');
 });
